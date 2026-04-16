3038dc9aa6a71d4d133dd9fee90f181e9c702fbd
VoVanThuan2004 <vot04766@gmail.com>
2026-04-16 15:38:34 +0700

    fix: Fixing fonts folder for download pdf

--- src/lib/pdf_font.ts
+++ src/lib/pdf_font.ts
@@ -4,22 +4,22 @@ Font.register({
   family: "Roboto",
   fonts: [
     {
-      src: "/src/assets/fonts/Roboto-VariableFont_wdth,wght.ttf",
+      src: "/fonts/Roboto-VariableFont_wdth,wght.ttf",
       fontWeight: "normal",
       fontStyle: "normal",
     },
     {
-      src: "/src/assets/fonts/Roboto-Italic-VariableFont_wdth,wght.ttf",
+      src: "/fonts/Roboto-Italic-VariableFont_wdth,wght.ttf",
       fontWeight: "normal",
       fontStyle: "italic",
     },
     {
-      src: "/src/assets/fonts/Roboto-VariableFont_wdth,wght.ttf",
+      src: "/fonts/Roboto-VariableFont_wdth,wght.ttf",
       fontWeight: "bold",
       fontStyle: "normal",
     },
     {
-      src: "/src/assets/fonts/Roboto-Italic-VariableFont_wdth,wght.ttf",
+      src: "/fonts/Roboto-Italic-VariableFont_wdth,wght.ttf",
       fontWeight: "bold",
       fontStyle: "italic",
     },
